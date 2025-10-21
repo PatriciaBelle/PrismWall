26d66516cada82486511bc58a7ed15c46d748e96
PatriciaBelle <franceskobismarkas@gmail.com>
2025-10-21 11:48:33 +0800

    deploy scripts

---
+++ deploy/deploy_prismwall.ts
@@ -0,0 +1,19 @@
+import type { DeployFunction } from "hardhat-deploy/types";
+import type { HardhatRuntimeEnvironment } from "hardhat/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  await deploy("PrismWall", {
+    from: deployer,
+    log: true,
+    args: [],
+  });
+};
+
+export default func;
+func.tags = ["PrismWall"];
+
+
